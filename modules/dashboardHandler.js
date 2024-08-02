4c18eb3067909bdd389d7b3ca1abd1d10dfe6368
Clement Osuide <debaosuidecl@gmail.com>
2024-08-02 11:25:42 +0100

    D

--- modules/dashboardHandler.js
+++
@@ -1,8 +0,0 @@
-function dashboardHandler(request,response){
-    response.statusCode = 200;
-    response.setHeader("Content-Type", "text/plain");
-    response.end("Welcome to the user dashboard")
-}
-
-
-module.exports = dashboardHandler
--- modules/notfoundHandler.js
+++
@@ -1,6 +0,0 @@
-function notFoundHandler (request,response){
-    response.statusCode = 404;
-    response.end("Page not found");
-}
-
-module.exports = notFoundHandler;
